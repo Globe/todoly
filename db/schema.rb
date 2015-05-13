b145419f90b47a155d984e1e771954cbad735ce7
Francis Plaza <francis@francis.ph>
2015-05-13 11:05:51 +0800

    added list

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511060326) do
+ActiveRecord::Schema.define(version: 20150512031714) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "description"
     t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "list_id"
   end
 
 end
